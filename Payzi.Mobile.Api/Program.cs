45528fe38f442b1475e30743128ee3a971350bc7
Antonio Sepúlveda <51375386+Sufferown@users.noreply.github.com>
2023-10-30 13:32:31 -0300

    Mod: Developer & Production
    Se agrega ambiente de producción

--- Payzi.Mobile.Api/Program.cs
+++ Payzi.Mobile.Api/Program.cs
@@ -66,7 +66,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.ConfigureSwagger();
 }
